3dcfec4c7640d2dec18d64d4aea65ded07ce87de
admin <admin@lianglili.local>
2018-07-03 14:12:44 +0800

    歌曲列表组件开始

--- src/components/player/player.vue
+++ src/components/player/player.vue
@@ -95,6 +95,7 @@
         </div>
       </div>
     </transition>
+    <playlist ref="playlist"></playlist>
     <audio ref="audio" :src="currentSong.url" @canplay="ready" @error="error" @timeupdate="updateTime"
            @ended="end"></audio>
   </div>
@@ -109,6 +110,7 @@
   import { shuffle } from 'common/js/utils'
   import Lyric from 'lyric-parser'
   import Scroll from 'base/scroll/scroll'
+  import Playlist from 'components/playlist/playlist'
 
   const transform = prefixStyle('transform')
   const transitionDuration = prefixStyle('transitionDuration')
@@ -456,7 +458,8 @@
     components: {
       ProgressBar,
       ProgressCircle,
-      Scroll
+      Scroll,
+      Playlist
     }
   }
 </script>
